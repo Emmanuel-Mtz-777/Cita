02308b5ef93a85305a50917fce4782f27d1d7b2d
AloValdes <alo.val.mora@outlook.com>
2024-10-10 07:55:00 -0600

    Pantalla Login, se modificaron los colores, falta modificar que no se rompa cuando se gire la pantalla

--- app/src/main/java/com/example/citaproyect/views/Login.kt
+++ app/src/main/java/com/example/citaproyect/views/Login.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -32,107 +33,120 @@ fun Login(navController: NavController) {
     var rememberMe by remember { mutableStateOf(false) }
     var passwordVisible by remember { mutableStateOf(false) }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .background(colorResource(id = R.color.darkMidnightBlue)),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(colorResource(id = R.color.darkMidnightBlue))
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        //Espacio para el logo
-
-
-        // Campo de nombre de usuario
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Username") },
-            modifier = Modifier
-                //.fillMaxWidth()
-                .background(Color.White),
-            singleLine = true,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Done
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            //Espacio para el logo
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logo2),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .fillMaxWidth(0.5f) // Tamañito de la imagen
+                    .aspectRatio(1f) // Con esto no se deforma la imagen
+                    .padding(bottom = 20.dp), // Espaciado inferior
+                contentScale = ContentScale.Fit // Con esto no se corta la imagen
             )
-        )
 
-        Spacer(modifier = Modifier.height(30.dp))
+            // Campo de nombre de usuario
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier
+                    .background(Color.White),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
 
 // Campo de contraseña
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier
-                .background(Color.White),
-                //.fillMaxWidth(),
-            singleLine = true,
-            visualTransformation = if (passwordVisible)
-                VisualTransformation.None
-            else PasswordVisualTransformation(),
-            trailingIcon = {
-                val image = if (passwordVisible)
-                    painterResource(
-                        id = android.R.drawable.ic_menu_view) // icono para mostrar contraseña
-                else
-                    painterResource(
-                        id = android.R.drawable.ic_secure) // icono para ocultar contraseña
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier
+                    .background(Color.White),
+                singleLine = true,
+                visualTransformation = if (passwordVisible)
+                    VisualTransformation.None
+                else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val image = if (passwordVisible)
+                        painterResource(
+                            id = android.R.drawable.ic_menu_view
+                        ) // icono para mostrar contraseña
+                    else
+                        painterResource(
+                            id = android.R.drawable.ic_secure
+                        ) // icono para ocultar contraseña
 
-                Icon(
-                    painter = image,
-                    contentDescription =
-                    if (passwordVisible) "Hide password"
-                    else "Show password",
-                    modifier = Modifier
-                        .clickable { passwordVisible = !passwordVisible }
+                    Icon(
+                        painter = image,
+                        contentDescription =
+                        if (passwordVisible) "Hide password"
+                        else "Show password",
+                        modifier = Modifier
+                            .clickable { passwordVisible = !passwordVisible }
+                    )
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done,
                 )
-            },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done,
             )
-        )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Checkbox "Remember me"
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Checkbox(
-                checked = rememberMe,
-                onCheckedChange = { rememberMe = it }
-            )
-            Text(
-                text = "Remember me",
-                color = Color.White,
-                fontSize = 25.sp
-            )
-        }
-
-        Spacer(modifier = Modifier.height(15.dp))
-
-        // Botón de "Sign in"
-        Button(
-            onClick = { navController.navigate("Menu") },
-            //modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Sign in",
-                fontSize = 23.sp)
-        }
-
-        Spacer(modifier = Modifier.height(15.dp))
-
-        // Texto "Forgot password?"
-        Text(
-            text = "Forgot password?",
-            color = colorResource(id = R.color.carolinaBlue),
-            fontSize = 20.sp,
-            modifier = Modifier.clickable
-                {
+            // Checkbox "Remember me"
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = rememberMe,
+                    onCheckedChange = { rememberMe = it }
+                )
+                Text(
+                    text = "Remember me",
+                    color = Color.White,
+                    fontSize = 25.sp
+                )
             }
-        )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            // Botón de "Sign in"
+            Button(
+                onClick = { navController.navigate("Menu") },
+                //modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Sign in",
+                    fontSize = 23.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            // Texto "Forgot password?"
+            Text(
+                text = "Forgot password?",
+                color = colorResource(id = R.color.carolinaBlue),
+                fontSize = 20.sp,
+                modifier = Modifier.clickable{}
+            )
+        }
     }
 }
